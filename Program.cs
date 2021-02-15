bb2331412b882062e5ce346f730b8d8bba36da3d
Shiwanirana <shiwanirana40@gmail.com>
2021-02-15 15:33:53 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace rockpaperscissor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(ConsoleColor color in Enum.GetValues(typeof(ConsoleColor))){
+            Console.ForegroundColor = color ;
+            Console.WriteLine($"color: {color}");}
+            // Console.WriteLine("Hello World!");
+            List<string> options = new List<string>();
+            options.Add("Rock");
+            options.Add("Paper");
+            options.Add("Scissor");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Green;
+            // bool running = true;
+            // while(running){
+                Console.Clear();
+                Console.WriteLine(@"
+                
+  _____            _      _____                         _____      _                       _____                      
+ |  __ \          | |    |  __ \                       / ____|    (_)                     / ____|                     
+ | |__) |___   ___| | __ | |__) |_ _ _ __   ___ _ __  | (___   ___ _ ___ ___  ___  _ __  | |  __  __ _ _ __ ___   ___ 
+ |  _  // _ \ / __| |/ / |  ___/ _` | '_ \ / _ \ '__|  \___ \ / __| / __/ __|/ _ \| '__| | | |_ |/ _` | '_ ` _ \ / _ \
+ | | \ \ (_) | (__|   <  | |  | (_| | |_) |  __/ |     ____) | (__| \__ \__ \ (_) | |    | |__| | (_| | | | | | |  __/
+ |_|  \_\___/ \___|_|\_\ |_|   \__,_| .__/ \___|_|    |_____/ \___|_|___/___/\___/|_|     \_____|\__,_|_| |_| |_|\___|
+                                    | |                                                                               
+                                    |_|                                                                               
+
+                ");
+                Console.WriteLine(@"Rock-Paper-Scissor Game");
+                Console.WriteLine("Choose Your OPTION WISELY!!...COMPUTER can be EVIL Sometimes");
+                String userChoice = Console.ReadLine();
+                Console.WriteLine(userChoice);
+                Random r = new Random();
+                int comIndex = r.Next(3);
+                String comChoice = options[comIndex];
+                Console.WriteLine("And here comes the COMPUTER -->"+ comChoice);
+
+            // }
+            // while(running){
+
+
+            // }
+            var result = userChoice == comChoice?"You both fight with same TOOLS and DIE..because there will be no winner!!":userChoice=="Rock" && comChoice == "Paper"|| userChoice=="Paper" && comChoice=="Scissor"|| userChoice=="Scissor" && comChoice=="Rock"?"Computer is the WINNER!!":"User is the WINNER";
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
